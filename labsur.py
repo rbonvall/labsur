89cc5f9c00d8f2d630d85d900a41fbd507fdeed0
Roberto Bonvallet <rbonvall+github@gmail.com>
2008-10-15 00:50:32 -0300

    initial commit

---
+++ labsur.py
@@ -0,0 +1,242 @@
+#!/usr/bin/env python
+# vim: fileencoding=utf-8
+
+try:
+    from collections import namedtuple
+except ImportError:
+    from namedtuple import namedtuple
+from string import Template
+from datetime import datetime
+import pygtk
+pygtk.require20()
+import gtk
+
+CowStats = namedtuple('CowStats', ['nr_cow', 'mat', 'prot', 'nn', 'cells'])
+
+TEMPLATE_FILE = r'template'
+
+class CowStatsParseError(Exception):
+    pass
+
+def parse_cow_file(f):
+    cows = list()
+    for line in f:
+        try:
+           row = CowStats(*map(int, line.strip().split(',')))
+        except:
+            raise CowStatsParseError('Archivo invalido')
+        cows.append(row)
+    return cows
+
+
+class Application:
+    def __init__(self):
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.set_title('LabSur')
+        self.window.connect('delete_event', self.quit)
+        self.window.set_default_size(200, 400)
+
+        self.vbox = gtk.VBox()
+        self.vbox.show()
+        self.window.add(self.vbox)
+
+        self.button_box = gtk.HBox()
+        self.button_box.show()
+        self.vbox.pack_start(self.button_box, expand=False)
+
+        self.load_button = gtk.Button('Cargar datos')
+        self.load_button.connect('clicked', self.load, None)
+        self.button_box.pack_start(self.load_button)
+        self.load_button.show()
+
+        self.export_button = gtk.Button('Exportar')
+        self.export_button.connect('clicked', self.export, None)
+        self.button_box.pack_start(self.export_button)
+        self.export_button.show()
+
+        self.print_button = gtk.Button('Crear version imprimible')
+        self.print_button.connect('clicked', self.printable, None)
+        self.button_box.pack_start(self.print_button)
+        self.print_button.show()
+
+        self.quit_button = gtk.Button('Salir')
+        self.quit_button.connect('clicked', self.quit)
+        self.button_box.pack_start(self.quit_button)
+        self.quit_button.show()
+
+        self.form = gtk.Table(8, 4, False)
+        self.fields = {
+          'NUMEROSOLICITUD':self.add_form_item('Nro. solicitud', 0, 0),
+
+          'PROPIETARIO':    self.add_form_item('Propietario',    1, 0),
+          'RUT':            self.add_form_item('Rut',            2, 0),
+          'DIRECCION':      self.add_form_item('Direccion',      3, 0),
+          'COMUNA':         self.add_form_item('Comuna',         4, 0),
+          'SOLICITANTE':    self.add_form_item('Solicitante',    5, 0),
+          'NUMEROEXAMEN':   self.add_form_item('Nro. examen',    6, 0),
+          'FECHARECEPCION': self.add_form_item('Fecha recepcion',   0, 1),
+          'FECHAINFORME':   self.add_form_item('Fecha informe',     1, 1),
+          'CANTMUESTRAS':   self.add_form_item('Cantidad muestras', 2, 1),
+          'TECNICA':        self.add_form_item('Tecnica',           3, 1),
+          'REACTIVO':       self.add_form_item('Reactivo',          4, 1),
+          'CODIGOEXAMEN':   self.add_form_item('Codigo examen',     5, 1),
+
+          'NOMBRE':       self.add_form_item('Firma', 6, 1),
+          'CARGO':        self.add_form_item('Cargo', 7, 1),
+        }
+        tab_order = ['NUMEROSOLICITUD',
+            'PROPIETARIO', 'RUT', 'DIRECCION', 'COMUNA',
+            'SOLICITANTE', 'NUMEROEXAMEN', 
+            'FECHARECEPCION', 'FECHAINFORME', 'CANTMUESTRAS',
+            'TECNICA', 'REACTIVO', 'CODIGOEXAMEN', 
+            'NOMBRE', 'CARGO']
+        self.form.set_focus_chain([self.fields[key] for key in tab_order])
+        self.form.show()
+        self.vbox.pack_start(self.form)
+
+        self.data_box = gtk.HBox()
+        self.data_box.show()
+        self.vbox.pack_start(self.data_box, expand=True)
+
+        self.data_view = gtk.TreeView()
+        self.data_box.pack_start(self.data_view, expand=True)
+        self.data_view.show()
+
+        v = self.data_view.get_vadjustment()
+        self.sb = gtk.VScrollbar(adjustment=v)
+        self.sb.show()
+        self.data_box.pack_start(self.sb, expand=False)
+        #self.data_view.set_vadjustment(v)
+
+        self.cow_count_column = gtk.TreeViewColumn('#')
+        self.data_view.append_column(self.cow_count_column)
+        cell = gtk.CellRendererText()
+        self.cow_count_column.pack_start(cell, True)
+        self.cow_count_column.set_attributes(cell, text=0)
+
+        self.nr_cow_column = gtk.TreeViewColumn('Nro. Vaca')
+        self.data_view.append_column(self.nr_cow_column)
+        cell = gtk.CellRendererText()
+        self.nr_cow_column.pack_start(cell, True)
+        self.nr_cow_column.set_attributes(cell, text=1)
+
+        self.cells_column = gtk.TreeViewColumn('RCS')
+        self.data_view.append_column(self.cells_column)
+        cell = gtk.CellRendererText()
+        self.cells_column.pack_start(cell, True)
+        self.cells_column.set_attributes(cell, text=5)
+
+        self.data = None
+
+        self.window.show()
+
+
+    def add_form_item(self, label, row, col):
+        l = gtk.Label(label)
+        l.show()
+        self.form.attach(l, 2*col, 2*col+1, row, row+1)
+        e = gtk.Entry()
+        e.set_alignment(0.0)
+        e.show()
+        self.form.attach(e, 2*col+1, 2*col+2, row, row+1)
+        return e
+
+    def load(self, *args):
+        self.list_store = gtk.ListStore(int, int, int, int, int, int)
+        file_name = self.choose_data_source()
+        self.data = []
+        cow_file = open(file_name)
+        for i, cow in enumerate(parse_cow_file(cow_file)):
+            self.list_store.append((i + 1,) + cow)
+            self.data.append(cow)
+        self.data_view.set_model(self.list_store)
+        self.window.resize(200, 400)
+
+    def export(self, *args):
+        if not self.data:
+            print "No hay datos para exportar"
+            return
+        file_name = self.choose_export_name()
+        data_file = open(file_name, 'w')
+        for cow in self.data:
+            reg = "%-8s%-4s%-4s%-4s%1s%3s" % (cow.nr_cow, cow.mat, cow.cells, cow.prot, chr(10), 3 * "\n")
+            data_file.write(reg)
+        data_file.write('9' * len(reg)) # registro con nueves para marcar final
+        data_file.close()
+
+    def printable(self, *args):
+        if not self.data:
+            print "No hay datos para imprimir"
+            return
+        report_template = Template(open(TEMPLATE_FILE).read())
+        file_name = self.choose_report_name()
+        report_file = open(file_name, 'w')
+
+        report_data = {}
+        for k, v in self.fields.items():
+            report_data[k] = v.get_text()
+        report_data['TABLA'] = self.data_to_html()
+
+        report_file.write(report_template.substitute(report_data))
+        report_file.close()
+
+    
+    def choose_data_source(self):
+        return self.choose_file('Abrir archivo de datos',
+                                gtk.FILE_CHOOSER_ACTION_OPEN,
+                                extension='lab')
+    def choose_report_name(self):
+        return self.choose_file('Guardar informe como',
+                                gtk.FILE_CHOOSER_ACTION_SAVE,
+                                current_name='informe.html',
+                                extension='html',
+                                directory='Informes')
+    def choose_export_name(self):
+        return self.choose_file('Exportar datos como',
+                                gtk.FILE_CHOOSER_ACTION_SAVE,
+                                current_name='export.dat',
+                                extension='dat',
+                                directory='Datos')
+
+    def choose_file(self, title, action, current_name=None, extension=None, directory=None):
+        b = (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_OPEN, gtk.RESPONSE_OK)
+        chooser = gtk.FileChooserDialog(title=title, action=action, buttons=b)
+        if current_name:
+            chooser.set_current_name(current_name)
+        if extension:
+            f = gtk.FileFilter()
+            f.add_pattern('*.%s' % extension)
+            chooser.add_filter(f)
+        response = chooser.run()
+        if response == gtk.RESPONSE_OK:
+            name = chooser.get_filename()
+        else:
+            name = None
+        chooser.destroy()
+        return name
+
+    def data_to_html(self):
+        COLUMNS = 4
+        headers = ('Corr', 'Nro. vaca', 'RCS')
+        row_template = '<tr>' + 3 * '<td>%s</rd>' + '</tr>'
+        fields = [(i+1, cow.nr_cow, cow.cells) for i, cow in enumerate(self.data)]
+        rows = [row_template % field for field in fields]
+        tables = [rows[i::COLUMNS] for i in range(COLUMNS)]
+        for table in tables:
+            table.insert(0, row_template % headers)
+        tables = [''.join(table) for table in tables]
+        tables = ['<table class="cell-table">%s</table>' % table for table in tables]
+        return ''.join(tables)
+
+
+    def main(self):
+        gtk.main()
+    def quit(self, *args):
+        gtk.main_quit()
+        return False
+
+
+if __name__ == '__main__':
+    app = Application()
+    app.main()
+
